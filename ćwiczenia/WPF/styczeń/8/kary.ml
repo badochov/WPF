774e44ee14b29f5e64cca00577e6dd94f116401f
Hubert Badocha <badocha.hubert@gmail.com>
2020-03-16 19:57:23 +0100

    lepsze ćwiczenia

--- ćwiczenia/WPF/styczeń/8/kary.ml
+++
@@ -1,9 +0,0 @@
-(* 
-    Wykonanie każdego z nich zajmuje określoną liczbę dni.
-    Za każdy dzień opóźnienia wykonania każdego z zobowiązań firma płaci określone kary.
-    Napisz procedurę harmonogram która obliczy optymalną kolejność wykonywania zaległych zobowiązań.
-    Procedura ta otrzymuje listę trójek postaci: (nazwa czynności, liczba dni, kara za dzień zwłoki).
-    Jej wynikiem powinna być lista nazw czynności w kolejności, w jakiej należy je wykonywać.
-*)
-
-(* sortowanie po liczbie dni / kara za dzień *)
